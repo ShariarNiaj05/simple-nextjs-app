e7a5138b1ac86e0c4d0a883ad5916479513a52ae
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-31 01:13:21 +0600

    Navigation with link component & create built-in routes

---
+++ src/app/loading.js
@@ -0,0 +1,10 @@
+
+const loading = () => {
+    return (
+        <div className=" text-4xl text-center text-red-500">
+            loading........
+        </div>
+    );
+};
+
+export default loading;
