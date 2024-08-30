42ee9abe91e64d6706b4ec5e80cda273692cdacb
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-31 00:23:13 +0600

    server component to client component

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+"use client"
+import { useState } from "react";
 
 const HomePage = () => {
+  const [counter, setCounter] = useState(0)
+  console.log(counter);
   return (
     <div>
-      <h2 className="text-4xl font-bold">Home</h2>
+      <h2 className="text-4xl font-bold" >Home and {counter}</h2>
+      <button onClick={() => setCounter(counter + 1)}>Increase</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrease</button>
     </div>
   );
 };
