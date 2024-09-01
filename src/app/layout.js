9e5564c76182ba0ecf07ec3c4b3bf8698c36728c
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-01 20:44:09 +0600

    font optimization

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,10 @@
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Shared/Navbar";
 
-const inter = Inter({ subsets: ["latin"], display: "swap" });
+// const inter = Inter({ subsets: ["latin"], display: "swap" });
+const roboto = Roboto({ weight: "400", subsets: ["latin"], display: "swap" });
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Navbar />
         {children}
       </body>
