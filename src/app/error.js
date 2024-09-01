16f3eff26933755d3db8b027818e894c47cc0f06
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-02 00:33:36 +0600

    Handle global errors with built-in error page

--- src/app/error.js
+++ src/app/error.js
@@ -4,6 +4,8 @@ const ErrorPage = ({ error, reset }) => {
         <div>
             Error Page.....
             <h1>{error.message}</h1>
+
+            <button onClick={() => reset()}>Try Again</button>
         </div>
     );
 };
