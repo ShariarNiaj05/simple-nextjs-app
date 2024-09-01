16e1f913d9218963118b894c73724c071f2dbb3c
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-02 00:30:36 +0600

    built in error page

--- src/app/error.js
+++ src/app/error.js
@@ -1,8 +1,9 @@
 "use client"
-const ErrorPage = () => {
+const ErrorPage = ({ error, reset }) => {
     return (
         <div>
             Error Page.....
+            <h1>{error}</h1>
         </div>
     );
 };
