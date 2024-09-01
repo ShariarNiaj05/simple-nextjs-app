6d2c07005fcb6c10bb70e568256ce362140c6f47
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-01 23:23:20 +0600

    data fetching

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,10 @@ export const metadata = {
   title: "my website's home page",
   description: "Home page description"
 }
-const HomePage = () => {
+const HomePage = async () => {
+  const res = await fetch("http://localhost:5000/shoes")
+  const data = await res.json()
+  console.log(data);
   return (
     <div>
       <h2 className="text-4xl font-bold" >Home</h2>
