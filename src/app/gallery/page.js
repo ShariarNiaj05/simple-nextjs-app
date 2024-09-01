0949a7079407c9fc4f2557e6eaba37f3da61335d
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-01 19:55:49 +0600

    image optimization

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -3,6 +3,9 @@ const GalleryPage = () => {
     return (
         <div>
             image optimizations
+            <h2>Regular image tag</h2>
+
+            <img src="https://nextjs.org/api/docs-og?title=API%20Reference:%20next.config.js%20Options" />
         </div>
     );
 };
