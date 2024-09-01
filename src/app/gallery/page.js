0adead84a25dcba3bb02a68ec4e4f3215b045127
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-01 20:25:06 +0600

    image optimization

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
-
+import a from "../../assets/a.jpg"
+import b from "../../assets/b.jpg"
+import c from "../../assets/c.jpg"
+import d from "../../assets/d.jpg"
+import e from "../../assets/e.jpg"
 const GalleryPage = () => {
     return (
         <div>
@@ -10,6 +14,11 @@ const GalleryPage = () => {
 
             <h2>Next js image component</h2>
             <Image src={"https://nextjs.org/api/docs-og?title=API%20Reference:%20next.config.js%20Options"} alt="next image" width={500} height={500} />
+            <Image src={a} alt="next image" width={500} height={500} />
+            <Image src={b} alt="next image" width={500} height={500} />
+            <Image src={c} alt="next image" width={500} height={500} />
+            <Image src={d} alt="next image" width={500} height={500} />
+            <Image src={e} alt="next image" width={500} height={500} />
         </div>
     );
 };
