81fdaadd599438b149505d4b91e7e57934c2e866
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-01 20:13:56 +0600

    image optimization

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -9,7 +9,7 @@ const GalleryPage = () => {
             <img src="https://nextjs.org/api/docs-og?title=API%20Reference:%20next.config.js%20Options" />
 
             <h2>Next js image component</h2>
-            <Image src={"https://nextjs.org/api/docs-og?title=API%20Reference:%20next.config.js%20Options"} alt="next image" width={500} height={500} />
+            <Image src={"https://nextjs.org/api/docs-og?title=API%20Reference:%20next.config.js%20Options"} fill alt="next image" />
         </div>
     );
 };
