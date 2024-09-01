bab5c6758f8d2669c39213c60cb79bbbcfb8fe7f
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-01 23:27:15 +0600

    data fetching

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,33 @@ export const metadata = {
 }
 const HomePage = async () => {
   const res = await fetch("http://localhost:5000/shoes")
-  const data = await res.json()
-  console.log(data);
+  const shoes = await res.json()
   return (
     <div>
       <h2 className="text-4xl font-bold" >Home</h2>
       <Counter />
-
+      <div>
+        {
+          shoes?.map(shoe => <div key={shoe.id} className="card bg-base-100 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                !
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Fashion</div>
+                <div className="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>)
+        }
+      </div>
     </div>
   );
 };
